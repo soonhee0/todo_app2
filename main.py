7ab1f893d6c622d59876245bfd22c9a902c2a551
soonhee0 <kyoujunki0@gmail.com>
2024-05-21 13:42:18 +0900

    requirements.txtファイルの更新完了。Webサーバーが起動したら、dbが作成される設定。importエラーが出ている。一時的なコミット

--- main.py
+++ main.py
@@ -1,7 +1,54 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,Depends
+from sqlalchemy.orm import Session
+# from .settings import engine, Base, SessionLocal
+from .models import Task,Base
+from .database import engine
+# FastAPIアプリケーションの作成
 app = FastAPI()
+
+
+
+# FastAPIのStartupイベントでテーブルを作成する
+@app.on_event("startup")
+def startup_event():
+    # テーブルを作成
+    Base.metadata.create_all(bind=engine)
+
+# get_db は、SQLAlchemyのセッションを取得し、使用後に確実にクローズするためのジェネレータ関数
+# データベースとの接続を取得する依存関係　関数やクラスが必要とする依存オブジェクトを外部から提供する
+def get_db():
+    # 新しいデータベースセッションを作成し、変数dbに割り当てる。
+    db = SessionLocal()
+    try:
+        # セッションオブジェクトを呼び出しもとに返す　この時関数の実行は一時停止する
+        yield db
+    finally:
+        # 呼び出し元の処理が終わるとsessionが確実に閉じられる
+        db.close()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+# # タスク作成エンドポイントの定義
+# @app.post("/tasks/")
+# async def create_task(title: str, description: str, db: Session = Depends(get_db)):
+#     # Taskオブジェクトを作成してセッションに追加
+#     db_task = Task(title=title, description=description)
+#     db.add(db_task)
+#     db.commit()
+#     db.refresh(db_task)
+#     return db_task
+
+# # タスク一覧取得エンドポイントの定義
+# @app.get("/tasks/")
+# async def read_tasks(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+#     tasks = db.query(Task).offset(skip).limit(limit).all()
+#     return tasks
+
+# Pythonスクリプトが直接実行された場合
+if __name__ == "__main__":
+    import uvicorn
+    # uvicornを使ってサーバーを起動するためのコード
+    # host="0.0.0.0"はサーバーがすべてのネットワークインターフェースで待ち受けることを意味する
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- setting.py
+++ setting.py
@@ -1,54 +1,19 @@
-# SQLAlchemyを使ってデータベースのテーブルを定義する
-from typing import List,Optional
-from sqlalchemy import ForeignKey, String, Integer, Date, create_engine
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy.orm import declarative_base
+# 　環境変数のロードをするファイル
+from sqlalchemy import create_engine
+# from typing import List,Optional
+
+# from sqlalchemy.orm import declarative_base
+# from sqlalchemy.orm import sessionmaker
 from sqlalchemy.inspection import inspect
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
+from .database import engine, Base
 import os
 
-
-# .env ファイルをロード
-load_dotenv()
-
-
-# 環境変数からDATABASE_URLを取得する
-DATABASE_URL = os.getenv("DATABASE_URL")
-# PostgreSQLデータベースに接続するためのengineオブジェクトを作成する
-engine = create_engine(DATABASE_URL)
-
-print(f"DATABASE_URL: {DATABASE_URL}")
-
-# ベースクラスを定義する
-class Base(DeclarativeBase):
-    pass
-
-# テーブルを定義するための基本クラスを定義する
-Base = declarative_base() 
-
-# テーブルを表すクラスを定義する。
-class Status(Base):
-    __tablename__ = "status"
-    id=mapped_column(Integer, primary_key=True, autoincrement=True)
-    display_name=mapped_column(String,nullable=False)
-     
-     # ... mapped_column() mappings taskテーブルとのリレーションシップ
-    tasks: Mapped[List["Task"]] = relationship("Task",back_populates="status")
-
-class Task(Base):
-    __tablename__ = "task"
-    id=mapped_column(Integer,primary_key=True,autoincrement=True)
-    title=mapped_column(String(30), nullable=False)
-    deadline:Mapped[Optional[Date]]=mapped_column(Date, nullable=True)
-    status_id=mapped_column(Integer,ForeignKey('status.id'))
-   
-     # ... mapped_column() mappings Pythonクラス間のリレーションを定義し、オブジェクト間の関連性を扱いやすくする
-
-    status: Mapped["Status"] = relationship("Status",back_populates="task")
 
 # エンジンにベースクラスを関連付ける。 定義されたテーブルをデータベースに作成する
 # metadataとは、データベースの様々な情報を保持しているオブジェクト
 Base.metadata.create_all(engine)
+
 
 
 # テーブルの存在を確認する
